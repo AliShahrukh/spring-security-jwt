30d33f4ca7e10074aa1aafcd81207bd0e6636566
AliShahrukh <shahrukh.ali7860@gmail.com>
2020-07-27 12:55:00 +0530

    UserDetailsService

---
+++ src/main/java/com/ali/springsecurityjwt/service/JwtUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.ali.springsecurityjwt.service;
+
+import com.ali.springsecurityjwt.model.User;
+import com.ali.springsecurityjwt.model.UserDto;
+import com.ali.springsecurityjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder bCryptEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+    public User save(UserDto user) {
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(bCryptEncoder.encode(user.getPassword()));
+        return userRepository.save(newUser);
+    }
+}
